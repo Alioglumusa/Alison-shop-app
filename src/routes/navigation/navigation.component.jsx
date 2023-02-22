64fa2d102bf7edebee91343149a699fb29198646
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2023-02-22 23:08:20 +0300

    firebase - Authenticating With Firebase

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,22 +1,22 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
-import {ReactComponent as CrwnLogo} from "../../assets/crown.svg"
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
   return (
     <Fragment>
-      <div className="navigation" >
+      <div className="navigation">
         <Link className="logo-container" to="/">
-        <CrwnLogo className="logo" />
+          <CrwnLogo className="logo" />
         </Link>
         <div className="links-container">
-            <Link className="nav-link" to="/shop" >
-                SHOP
-            </Link>
-            <Link className="nav-link" to="/sign-in" >
-                SIGN IN
-            </Link>
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
         </div>
       </div>
       <Outlet />
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDU1UiuWJg8v8qy_kgUoeTuP0byLU5Gn-8",
+  authDomain: "crwn-clothing-db-8d228.firebaseapp.com",
+  projectId: "crwn-clothing-db-8d228",
+  storageBucket: "crwn-clothing-db-8d228.appspot.com",
+  messagingSenderId: "1081571271823",
+  appId: "1:1081571271823:web:82d77121e3cb2c6ac1bcfa",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
