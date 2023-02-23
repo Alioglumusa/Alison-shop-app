3dacd3f48870fdf563a23cdb4b20aaf5f31ae01d
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2023-02-23 23:50:27 +0300

    firebase - Setting Up User Documents

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDU1UiuWJg8v8qy_kgUoeTuP0byLU5Gn-8",
   authDomain: "crwn-clothing-db-8d228.firebaseapp.com",
@@ -25,3 +27,14 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+ };
