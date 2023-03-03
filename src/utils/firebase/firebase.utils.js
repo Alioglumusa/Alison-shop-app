f39da5d981a63f4f3e95371ebff4393839e9b769
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2023-03-03 03:13:07 +0300

    Observer Pattern - errorCallback,completedCallback

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -87,4 +87,6 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) =>
-  onAuthStateChanged(auth, callback);
+  onAuthStateChanged(auth, callback, errorCallback, completedCallback);
+
+
