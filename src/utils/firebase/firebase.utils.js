4a5bffa276a1499a182368bf7c924c3f53b2b1b3
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2023-03-03 03:13:43 +0300

    /--/

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -87,6 +87,6 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) =>
-  onAuthStateChanged(auth, callback, errorCallback, completedCallback);
+  onAuthStateChanged(auth, callback);
 
 
