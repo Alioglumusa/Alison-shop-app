cec7e6ced032d28c0e5e5359ab798561d5b0e2f2
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2023-03-05 04:31:25 +0300

    Context - Cart Icon & Dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,9 @@ import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss";
 import { UserContext } from "../../context/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
+
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
@@ -28,7 +31,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown/>
       </div>
       <Outlet />
     </Fragment>
