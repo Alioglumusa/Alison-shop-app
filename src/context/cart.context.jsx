850964aaaf3ec2be1e5e7098dc9e6ee58b582d09
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2023-03-07 23:55:22 +0300

    Context - Add To Cart Pt-1

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,13 +1,22 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
   const value = { isCartOpen, setIsCartOpen };
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
