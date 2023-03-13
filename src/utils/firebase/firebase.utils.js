3d1c8d157475e4bff2d702e8aa1835c202f3dfb7
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2023-03-13 23:43:01 +0300

    Firebase - Get Produtcs +Categories From Firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -17,6 +17,8 @@ import {
   setDoc,
   collection,
   writeBatch,
+  query,
+  getDocs
 } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -60,6 +62,20 @@ export const addCollectionAndDocuments = async (
   await batch.commit()
   console.log("done"); 
 };
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, "categories")
+  const q = query(collectionRef)
+
+  const querySnapshot = await getDocs(q)
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+    const { title,items } = docSnapshot.data()
+    acc[title.toLowerCase()] = items
+    return acc
+  },{})
+  
+  return categoryMap
+}
 
 export const createUserDocumentFromAuth = async (
   userAuth,
