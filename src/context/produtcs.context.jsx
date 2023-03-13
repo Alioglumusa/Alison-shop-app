69f6ccd164f138927591140d136bd3fb9d7177d3
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2023-03-13 23:29:02 +0300

    Firebase Database Storage - addCollectionAndDocm.

--- src/context/produtcs.context.jsx
+++ src/context/produtcs.context.jsx
@@ -1,12 +1,13 @@
-import { createContext, useState } from "react";
-import PRODUCTS from "../shop-data.json";
+import { createContext, useState, useEffect } from "react";
+
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
