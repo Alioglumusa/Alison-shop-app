79543217f4ee4f11c15cf1b1d9c45ea0c5bbd749
Musa Alioğlu <109859611+Alioglumusa@users.noreply.github.com>
2023-03-14 01:29:57 +0300

    Firebase - Nested Routes in Shop

--- src/routes/shop/shop.component.js
+++ src/routes/shop/shop.component.js
@@ -1,22 +1,12 @@
-import { useContext, Fragment } from "react";
-import { CategoriesContext } from "../../context/categories.context";
-import CategoryPreview from "../../components/category-preview/category-preview.component";
-import ProductCard from "../../components/product-card/product-card.component";
-
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+    </Routes>
   );
 };
 
